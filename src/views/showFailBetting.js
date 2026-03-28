42f655b76ffe797bf9495c406d42b774d5506c41
rahwan10 <donggeon10@naver.com>
2026-03-28 19:02:02 +0900

    fix: 색상결과에 맞는 색 출력

--- src/views/showFailBetting.js
+++ src/views/showFailBetting.js
@@ -1,4 +1,4 @@
 export default function showFailBetting(rouletteColor,bettingMoney) {
     const resultBox = document.getElementById("result-content");
-    resultBox.innerHTML = `룰렛 결과: ${rouletteColor} <br>베팅 실패! -${Number(bettingMoney).toLocaleString()}원`;
+    resultBox.innerHTML = `룰렛 결과:<span style="background-color: ${rouletteColor}">${rouletteColor}</span> <br>베팅 실패! -${Number(bettingMoney).toLocaleString()}원`;
 }
--- src/views/successBetting.js
+++ src/views/successBetting.js
@@ -1,4 +1,4 @@
 export default function successBetting(rouletteColor,bonusMoney) {
     const ResultBox = document.getElementById("result-content");
-    ResultBox.innerHTML = `룰렛 결과: ${rouletteColor} <br>베팅 성공! +${bonusMoney.toLocaleString()}원`;
+    ResultBox.innerHTML = `룰렛 결과: <span style="background-color: ${rouletteColor}">${rouletteColor}</span> <br>베팅 성공! +${bonusMoney.toLocaleString()}원`;
 }
